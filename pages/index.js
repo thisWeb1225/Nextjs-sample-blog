7a0a00ea5ddc9038eab21161ae127742938b6128
thisWeb1225 <shan881225@gmail.com>
2023-05-03 14:07:00 +0800

    :bug: Fix: Fixed the Date component

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
-import Date from '../components/date';
 
 <Head>
   <title>Create Next App</title>
@@ -26,10 +25,7 @@ export default function Home({ allPostsData }) {
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
+              <hr />
             </li>
           ))}
         </ul>
