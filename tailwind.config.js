4a1c0a8666016b706b1b87011cd2468472778bde
thisWeb1225 <shan881225@gmail.com>
2023-05-30 15:07:13 +0800

    :wrench: feature: add some custom tailwind properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      "tw-dark": "#23232d",
-      "tw-primary": "#2ecce0",
-      "tw-white": "#fff",
-      "tw-gray": "#aaa",
+    extend: {
+      colors: {
+        'tw-dark': '#23232d',
+        'tw-primary': '#2ecce0',
+        'tw-white': '#fff',
+        'tw-gray': '#aaa',
+      },
+      boxShadow: {
+        'tw-shadow': '4px 4px 16px rgba(0, 0, 0, 0.8)',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
     },
-    boxShadow: {
-      "tw-shadow": "4px 4px 16px rgba(0, 0, 0, 0.8)"
-    },
-    extend: {},
   },
-  plugins: [
-    'tailwindcss',
-    'autoprefixer',
-  ],
-}
-
+  plugins: ['tailwindcss', 'autoprefixer'],
+};
