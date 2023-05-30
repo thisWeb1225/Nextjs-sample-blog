01a7949912477a9784130c3c0eff40f97db38b63
thisWeb1225 <shan881225@gmail.com>
2023-05-30 14:59:36 +0800

    :lipstick: Style: Add the hover effect in background

--- components/posts/postItem.tsx
+++ components/posts/postItem.tsx
@@ -1,12 +1,29 @@
 import Link from "next/link";
 import { sortedPostsDataType } from "../../lib/posts";
+import { useEffect, useRef } from "react";
+
+import  styles  from "./../../styles/postItem.module.css";
 
 const PostItem = ({ id, date, title }: sortedPostsDataType) => {
+  const post = useRef<HTMLLIElement>(null);
+
+  useEffect(() => {
+    post.current.style.background = `radial-gradient(circle at var(--x) var(--y),#2ecbe030, transparent 60%);`
+
+    post.current.addEventListener('mousemove', (e) => {
+      const postRect = post.current.getBoundingClientRect();
+      const calX = e.clientX - postRect.left;
+      const calY = e.clientY - postRect.top;
+      post.current.style.setProperty('--x', `${calX}px`);
+      post.current.style.setProperty('--y', `${calY}px`);
+    })
+  }, [post])
+  
 
   return (
-    <li key={id} className="min-h-[360px] text-center border-2 rounded-md border-tw-gray">
-      <Link href={`/posts/${id}`} className="h-full p-8 flex flex-col justify-center">
-        <h3 className="text-base">{title}</h3>
+    <li key={id} className={`min-h-[360px] text-center border-[1px] border-neutral-700 hover:border-tw-gray duration-500 rounded-md group relative ${styles.postItem}`} ref={post}>
+      <Link href={`/posts/${id}`} className="h-full p-8 flex flex-col justify-center relative z-10">
+        <h3 className="text-base text-gray-600 duration-500 group-hover:text-tw-white">{title}</h3>
         <p className="text-xs text-tw-gray">{date}</p>
       </Link>
     </li>
--- components/posts/posts.tsx
+++ components/posts/posts.tsx
@@ -5,11 +5,11 @@ const Posts = ({ allSortedPostsData }: {
   allSortedPostsData: sortedPostsDataType[]
 }) => {
   return (
-    <div className="mt-48 px-2 sm:px-8 md:px-24 lg:px-32">
-      <h3 className="text-4xl text-center">My Posts</h3>
-      <ul className="grid gap-8 grid-cols-3 mt-10">
+    <div className="mt-12">
+      <h3 className="text-lg text-tw-gray mx-2 sm:mx-8 md:mx-24 lg:mx-32 border-b-[1px] pb-2 border-gray-600">My Posts</h3>
+      <ul className="grid grid-cols-3 gap-8 mt-10 px-2 sm:px-8 md:px-24 lg:px-32">
         {allSortedPostsData.map(({ id, date, title }) => (
-          <PostItem id={id} date={date} title={title}/>
+          <PostItem id={id} date={date} title={title} key={id}/>
         ))}
       </ul>
     </div>
