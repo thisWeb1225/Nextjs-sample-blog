776dc1ac53903bbc34dc69e72f31a468452e5ab7
thisWeb1225 <shan881225@gmail.com>
2023-06-15 23:36:38 +0800

    :recycle: Refactor: Add img parameter for hover function

--- components/project/projectItem.tsx
+++ components/project/projectItem.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 import Link from "next/link";
 
 import { useEffect, useRef } from "react";
-import { MouseEvent } from "react";
+import { MouseEvent, MutableRefObject } from "react";
 
 import { ProjectTypeWithHtml } from "../../lib/projects";
 /**
@@ -23,19 +23,19 @@ type PropType = Omit<ProjectTypeWithHtml, 'contentHtml'>;
 const ProjectItem = ({bannerSrc, id, title, role, date}: PropType) => {
   const img = useRef<HTMLImageElement>();
 
-  const showImg = (e:MouseEvent<HTMLDivElement>) => {
+  const showImg = (img:MutableRefObject<HTMLImageElement>, e:MouseEvent<HTMLDivElement> ) => {
     if (!img.current) return;
     const targetRect = e.currentTarget.getBoundingClientRect();
-    const x = e.clientX - targetRect.left;
-    const y = e.clientY - targetRect.top;
+    const x = Math.round(e.clientX - targetRect.left);
+    const y = Math.round(e.clientY - targetRect.top);
 
     img.current.style.translate = `${x - 120}px ${y - 120}px`
   }
 
   return (
     <Link href={`/projects/${id}`} >
-      <div className="flex py-12 border-b-[1px] border-tw-gray justify-between items-center relative group" onMouseMove={showImg}>
-        <div className="bg-tw-gray w-[240px] aspect-square grid place-content-center opacity-0 scale-0 absolute top-0 left-0 duration-500 ease-linear group-hover:opacity-100 group-hover:scale-100 pointer-events-none" ref={img}>
+      <div className="flex py-12 border-b-[1px] border-tw-gray justify-between items-center relative group" onMouseMove={(e) => showImg(img, e)}>
+        <div className="bg-tw-gray w-[240px] aspect-square grid place-content-center opacity-0 scale-0 absolute top-0 left-0 duration-300 ease-linear group-hover:opacity-100 group-hover:scale-100 pointer-events-none z-10" ref={img}>
           <Image
             src={bannerSrc}
             alt={title}
