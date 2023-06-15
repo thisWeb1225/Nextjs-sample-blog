2d3cc2ba6c2bc7e67a21a89496d9ffd4791b6c6a
thisWeb1225 <shan881225@gmail.com>
2023-06-15 23:35:02 +0800

    :recycle: Refactor: change the dependency of useEffect

--- hooks/useFollowMouseEffect.tsx
+++ hooks/useFollowMouseEffect.tsx
@@ -11,26 +11,26 @@ const useFollowMouseEffect = (
     }
 
     // set the transform for every child
-    children.forEach((child) => child.current.style.setProperty('transform', 'translateX(var(--x)) translateY(var(--y))'));
+    children.forEach((child) => child.current?.style.setProperty('transform', 'translateX(var(--x)) translateY(var(--y))'));
 
     // when enter, set the parentRect and children time function to linear
     function enterEffect() {
-      children.forEach((child) => child.current.style.setProperty('transition', `transform 0.5s linear`));
+      children.forEach((child) => child.current?.style.setProperty('transition', `transform 0.5s linear`));
     }
 
     function moveEffect(e: MouseEvent<Element, MouseEvent>) {
       const parentRect = parent.current.getBoundingClientRect();
-      const x = (e.clientX - parentRect.x - (parentRect.width / 2)) / 1.8;
-      const y = (e.clientY - parentRect.y - (parentRect.height / 2)) / 1.8;
+      const x = Math.round((e.clientX - parentRect.x - (parentRect.width / 2)) / 1.8);
+      const y = Math.round((e.clientY - parentRect.y - (parentRect.height / 2)) / 1.8);
 
       children.forEach((child, i) => {
-        child.current.style.setProperty('transform', `translate(${x / (i + 1)}px, ${y / (i + 1)}px)`);
+        child.current?.style.setProperty('transform', `translate(${x / (i + 1)}px, ${y / (i + 1)}px)`);
       });
     }
 
     function leaveEffect() {
       children.forEach((child) => {
-        child.current.style.setProperty('transition', 'transform 0.8s cubic-bezier(.50,1.8,.50,.8)');
+        child.current.style.setProperty('transition', 'transform 0.6s cubic-bezier(.50,1.8,.50,.8)');
         child.current.style.setProperty('transform', `translate(${0}px, ${0}px)`);
       });
     }
@@ -39,8 +39,16 @@ const useFollowMouseEffect = (
     parent.current.addEventListener('mousemove', moveEffect as any);
     parent.current.addEventListener('mouseout', leaveEffect as any);
 
-  }, [parent, ...children])
+    return () => {
+      if (parent.current) {
+        parent.current.removeEventListener('mouseenter', enterEffect as any);
+        parent.current.removeEventListener('mousemove', moveEffect as any);
+        parent.current.removeEventListener('mouseout', leaveEffect as any);
+      }
+    }
+
+  }, [parent.current, ...children.map((child) => child.current)])
 
 }
 
-export default useFollowMouseEffect
+export default useFollowMouseEffect;
