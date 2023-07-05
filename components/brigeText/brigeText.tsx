360a383103607a764ff6d88dea77c0919fb3d49d
thisWeb1225 <shan881225@gmail.com>
2023-07-05 18:05:46 +0800

    :recycle: Refactor: extract the brige text

---
+++ components/brigeText/brigeText.tsx
@@ -0,0 +1,44 @@
+import { useRef } from 'react';
+import useIsomorphicLayoutEffect from '../../hooks/useIsomorphicLayoutEffect';
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+type BrigeTextType = {
+  content: string
+}
+
+const BrigeText = ({ content }: BrigeTextType) => {
+
+  const text = useRef();
+
+  useIsomorphicLayoutEffect(() => {
+
+    let ctx = gsap.context(() => {
+
+      gsap.timeline({
+        scrollTrigger: {
+          trigger: text.current,
+          start: "top bottom",
+          end: "top 80%",
+          scrub: 1,
+        }
+      })
+        .from(text.current, {
+          y: 100,
+          opacity: 0,
+        })
+    })
+
+    return () => ctx.revert(); // cleanup
+
+  }, []); // <- empty dependency Array so it doesn't re-run on every render
+
+  return (
+    <p className="text-base text-center text-tw-gray my-52" ref={text}>{content}</p>
+  )
+}
+
+export default BrigeText
