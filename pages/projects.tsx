87790ef9734c020cd03332269c9ee9e1dff529ff
thisWeb1225 <shan881225@gmail.com>
2023-07-08 22:58:53 +0800

    :bug: Fix: Fixed the bug with the proejct page heading

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -4,6 +4,8 @@ import { getSortedProjectsData, ProjectTypeWithHtml } from "../lib/projects";
 import RootLayout from "../components/rootLayout/index";
 import ProjectContainer from "../components/project/index"
 
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
 type Props = {
   allSortedProjectsData: ProjectTypeWithHtml[],
 }
@@ -18,10 +20,11 @@ const Projects: React.FC = ({allSortedProjectsData}: Props) => {
 }
 
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async ({locale}) => {
   const allSortedProjectsData = getSortedProjectsData();
   return {
     props: {
+      ...(await serverSideTranslations(locale, ['common'])),
       allSortedProjectsData
     }
   }
